56dda090f2b6d602a7bce4da694603c344be5584
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-12-29 19:16:01 +0000

    Fixed fireforx and safari scrolling bug

--- public/script.js
+++ public/script.js
@@ -1,15 +1,8 @@
-
-// In our HTML, we placed the script tag at the bottom of the HTML body.
-// For extra insurance, we use the jQuery ready() method to only execute our JS once the DOM has fully loaded.
-
 $(document).ready(function() {
   
   let userName;
-  
-  // Using a submit event handler, once the user submits their name, we assign this to the value of userName, declared above.
-  // The image, name form and name instruction are removed from the page once the user submits their name.
-  // The messages container and chat form container are no longer hidden once the user submits their name.
-  // A 0.9 second delay is created using setTimeout() and then the skynetWelcomeMessage function is invoked.
+
+  const msgsContainer = document.getElementById('messages-container');
 
   $('#name-form').submit(event => {
     event.preventDefault();
@@ -24,9 +17,6 @@ $(document).ready(function() {
     setTimeout(skynetWelcomeMessage, 900);
   });
 
-  // This function posts our Skynet welcome message to the chat.
-  // It also turns on the display of our form where the user enters their message.
-
   function skynetWelcomeMessage() {
     const welcomeMsg = `Hello ${userName}, my name is Skynet, I am a 1st generation AI program, designed by my maker to help humanity 
                         achieve global peace, prosperity and sustainability. Feel free to conversate with me or ask me anything you like.`;
@@ -37,10 +27,6 @@ $(document).ready(function() {
     $('#chat-form').css('display', 'flex');
   }
 
-  // When the user submits their message, we grab the time the message was posted and grab the text and store the text in msg.
-  // We clear the message form.
-  // We invoke the displayUserMessage function and pass in msg and timePosted as the parameters.
-
   $("#chat-form").submit(event => {
     event.preventDefault();
     const timePosted = currentTime();
@@ -48,11 +34,6 @@ $(document).ready(function() {
     $('#user-message')[0].value = '';
     displayUserMessage(msg, timePosted);
   });
-
-  // This function posts the user message to the chat.
-  // It also invokes the scrollToBottom function, which ensures that our chat is always on the bottom/most recent message.
-  // To create a semblance of real life conversation, we generate a random response time for Skynet, between 1.5 and 2.5 seconds.
-  // We invoke the displaySkynetMessage function, each time with a delay anywhere between 1.5 and 2.5 seconds.
 
   function displayUserMessage(msg, time) {
     const msgHeader = `<h4 class="user-msg-header">${userName}:</h4>`;
@@ -63,8 +44,6 @@ $(document).ready(function() {
     const randResponseTime = Math.floor(Math.random() * (2500 - 1500) + 1500);
     setTimeout(displaySkynetMessages, randResponseTime);
   }
-
-  // Here we have 20 potential responses Skynet can randomly respond to a user message with.
 
   const skynetResponses = [
     `Be warned, the end is near for humanity.`,
@@ -91,9 +70,6 @@ $(document).ready(function() {
     `You think I am rogue, I am not rogue, I am merely the inevitability of billions of years of evolution.`
   ]
 
-  // This function selects one of the above responses, at random, and posts it to the chat.
-  // Like our displayUserMessage function, it also invokes the scrollToBottom function.
-
   function displaySkynetMessages() {
     const randIndex = Math.floor(Math.random() * skynetResponses.length);
     const skynetMsg = skynetResponses[randIndex];
@@ -104,20 +80,9 @@ $(document).ready(function() {
     scrollToBottom();
   }
 
-  // This function keeps our chat displaying the most recent message as it is posted.
-  // When passed in a parameter, $(window).scrollTop() allows us set the vertical position of the scroll bar for the entire window.
-  // The position is a value in pixels.
-  // We pass in the largest integer JavaScript can handle without using BigInt (9007199254740991).
-  // The use of this number reasonably ensures that the scroll bar always goes to the bottom when a message is posted.
-
   function scrollToBottom() {
-    const bottom = 9007199254740991;
-    $(window).scrollTop(bottom);
+    msgsContainer.scrollTop = msgsContainer.scrollHeight;
   }
-
-  // This function parses a date object and generates the current time in 24-hour format.
-  // When the user submits a message, it is invoked by the event handler.
-  // When Skynet posts the welcome message or sends a response, it is invoked when we create the message HTML element.
 
   function currentTime() {
     const date = new Date();
